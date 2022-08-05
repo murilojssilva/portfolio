4834a2dc5ca8b2e7a47c36d84321ce578c699954
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2022-08-05 20:16:55 -0300

    Change border-top style in Skill

--- src/components/Skills/Skill/styles.ts
+++ src/components/Skills/Skill/styles.ts
@@ -12,7 +12,7 @@ export const SkillContainer = styled.div<SkillContainerProps>`
   gap: 1rem;
   box-shadow: 0 0 1em ${(props) => props.theme.colors.backgroundItens};
   border-radius: 0 0 8px 8px;
-  border: 5px solid transparent;
+  border-radius: 8px;
   border-top: 5px solid ${(props) => props.theme.colors.primary};
 
   transition: filter 0.1s;
